9d7b53b34311751896c0f2659644b4c380e3dd03
user <user@users-MacBook-Pro.local>
2025-02-17 11:06:31 -0500

    new

--- ToDOList/src/App.jsx
+++ ToDOList/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ToDolist from './ToDoList.jsx'
+function ToDoList() {
+  const [tasks, setTasks] = useState(["drink water", "meditate", "shower"]);
+  const [newTask, setNewTask] = useState('');
 
-function App() {
-  const [count, setCount] = useState(0)
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+    
+  }
+
+  function addTask(){
+
+  }
+
+  function deleteTask(index){
+
+  }
+
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className = "to-do-list">
+      <h1>To Do List</h1>
+    
+
+    <div>
+      <input
+        type= "text"
+        placeholder="enter task"
+        value = {newTask}
+        onChange={handleInputChange}/>
+        <button 
+        className = "add-button"
+        onClick={addTask}>
+          Add Task
+          </button>
+    </div>
+     </div>)
 }
 
 export default App
---
+++ ToDOList/src/ToDoList.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function ToDoList() {
+
+    return (
+        <div>
+            <h1>ToDoList</h1>
+        </div>
+    )
+}
+
+export default ToDoList
